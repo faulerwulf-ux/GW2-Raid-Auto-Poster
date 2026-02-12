d3e976c08a426ccc2deede36426efd9f3e8ae15e
Turtle <deven.westmoreland@outlook.com>
2026-02-11 20:58:13 -0600

    Remove discord_webhook.py from git tracking

--- discord_webhook.py
+++
@@ -1,7 +0,0 @@
-
-
-#for SHEL server
-#DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1466972578265567517/tab8YHeBa7WCGLipPRvW2AiR7ZmABo1hHbIwxKwMYdKg2QVxTSDm1RELaNqn3aqxgwsQ"
-
-#for test server
-DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1468805127539527903/oPS7izoHGnrN-t-u7od_DcAZ1YQnhFRPBnQoha2LybopQbYiUZz1A7_moY8mOzLeaBvD"
